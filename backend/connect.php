4141d91cd79f74e45ec5bdd397ac8d113bb29397
talesCPV <tales@flexibus.com.br>
2024-05-01 15:36:33 -0300

    by_script

--- backend/connect.php
+++ backend/connect.php
@@ -7,7 +7,7 @@
     $banco = decrip('5Ynq0n83)3/=/2n!r,not/,/3n)+3"L;ww!8]n4q%n/z@5/0'); // flexib52_db_estoque
     $conexao = new mysqli($servidor, $usuario, $senha, $banco);
 */
-    $conexao = new mysqli("108.179.253.230", "flexib52_tales", "Xspider0", "flexib52_db_estoque");
+    $conexao = new mysqli("108.179.253.230", "flexib52_tales", "Xspider0", "flexib52_pesqueiro");
     if (!$conexao){
         die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
     }    
--- backend/sql.php
+++ backend/sql.php
@@ -33,7 +33,7 @@
 
         /* PRODUTOS */
         "PROD-0" => 'CALL sp_view_prod(@access,@hash,"x00","x01","x02");', // FIELD,SIGNAL, VALUE
-        "PROD-1" => 'CALL sp_set_prod(@access,@hash,x00,x01,"x02","x03","x04","x05","x06","x07","x08","x09",x10,"x11","x12","x13");',
+        "PROD-1" => 'CALL sp_set_prod(@access,@hash,x00,"x01","x02","x03","x04","x05","x06","x07","x08","x09",x10,"x11","x12","x13");',
         "PROD-2" => 'CALL sp_del_prod(@access,@hash,x00);', // ID
         "PROD-3" => 'CALL sp_set_reserv_prod(@access,@hash,x00,x01,x02,"x03",x04);', // ID_PROD, ID_PROJ,ID_USER,QTD,PAGO      
         "PROD-4" => 'CALL sp_inventario(@access,@hash,x00,"x01","x02")', // ID_PROD, QTD, OPERAÇÃO
