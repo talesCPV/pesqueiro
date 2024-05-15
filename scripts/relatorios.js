1fe95ae0dd0090453f3facfbda14d7e7b9f553f6
talesCPV <tales@flexibus.com.br>
2024-05-15 15:25:14 -0300

    by_script

--- scripts/relatorios.js
+++ scripts/relatorios.js
@@ -16,18 +16,22 @@ function comanda_virual(comanda){
 
     doc.setFontSize(10);
     txt.y = logo.height/14 + 20
-    doc.text('Comanda Virtual',20,txt.y)
+    doc.text('Comanda Virtual',22,txt.y)
     addLine()
     line(txt.y)
-    addLine()
-    doc.text(`Cliente: ${comanda.nome}`,10,txt.y)
     addLine()
     doc.text(`Comanda: ${comanda.id.padStart(5,0)}`,10,txt.y)
     addLine()
     doc.text(`Data: ${comanda.data} ${comanda.hora}`,10,txt.y)
     addLine()
+    doc.text(`Cliente: ${comanda.nome}`,10,txt.y)
+    addLine()
     let base64Image = document.querySelector('#qr_code img').src
     doc.addImage(base64Image, 'png', 10, txt.y, 50, 50);
+    txt.y += 50
+    addLine()
+    doc.setFontSize(8);
+    doc.text(`Acompanhe sua comanda por este QR-Code`,6,txt.y)
 
     doc.save('comanda_virtual.pdf')
 }
