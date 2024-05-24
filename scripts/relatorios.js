f164bcbcb8f62df287b3eec401cb37f104f7d2c7
talesCPV <tales@flexibus.com.br>
2024-05-24 11:44:10 -0300

    by_script

--- scripts/relatorios.js
+++ scripts/relatorios.js
@@ -2,7 +2,7 @@
 function comanda_virual(comanda){
 
     const logo = new Image()
-    logo.src = 'assets/logo.png'
+    logo.src = 'assets/logo_peq.png'
 
 
     const doc = new jsPDF({
@@ -12,28 +12,29 @@ function comanda_virual(comanda){
     });
 
 
-    doc.addImage(logo, 'png', 14, 10, logo.width/10, logo.height/10);
+    doc.addImage(logo, 'png', 24, 10, 50, 15.5);
 
     doc.setFontSize(20);
-    txt.y = logo.height/10 + 20
+    txt.y = 35
     doc.text('Comanda Virtual',22,txt.y)
     addLine(2)
     line(txt.y)
     addLine()
     doc.setFontSize(36);
     doc.text(comanda.id.padStart(5,0),30,txt.y)
-    addLine(1.5)
+    addLine(2)
     doc.setFontSize(18);
     doc.text(`${comanda.data} - ${comanda.hora}`,17,txt.y)
-    addLine(3)
-    doc.text(comanda.nome.toUpperCase(),1,txt.y)
+    addLine(2)
+    doc.setFontSize(12);
+    doc.text(comanda.nome.toUpperCase(),5,txt.y)
     addLine(1)
     let base64Image = document.querySelector('#qr_code img').src
-    doc.addImage(base64Image, 'png', 1, txt.y, 95, 95);
-    txt.y += 95
+    doc.addImage(base64Image, 'png', 5, txt.y, 85, 85);
+    txt.y += 85
     addLine()
     doc.setFontSize(12);
-    doc.text(`Acompanhe sua comanda por este QR-Code`,6,txt.y)
+    doc.text(`Acompanhe sua comanda por este QR-Code`,5,txt.y)
 
     const blob = doc.output('blob')
     return uploadFile(blob,`config/user/${localStorage.getItem('id_user')}/temp/`,'comanda.pdf')
