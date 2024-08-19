cbe9d61c5b455a03bc41b347288a0bb7fddb5c34
talesCPV <tales@flexibus.com.br>
2024-08-19 12:04:53 -0300

    by_script

---
+++ backend/saveFile.php
@@ -0,0 +1,13 @@
+<?php   
+	if (IsSet($_POST["path"]) && IsSet($_POST["file"])){
+        $path = getcwd().$_POST["path"];
+        $file = $_POST["file"];
+//echo $file;
+        $fp = fopen($path, "w");
+        fwrite($fp,$file);
+        fclose($fp); 
+
+        print $file;
+    }        
+    
+?>
